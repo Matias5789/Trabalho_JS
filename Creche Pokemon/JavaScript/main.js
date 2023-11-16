89d5f8d56e83d5d29038bed9e39047939f016859
Danrley da Silva Regis <112701726+DanHunterz1@users.noreply.github.com>
2023-11-16 12:18:41 -0300

    Update main.js

--- Creche Pokemon/JavaScript/main.js
+++ Creche Pokemon/JavaScript/main.js
@@ -1,7 +1,22 @@
 const removeproduto = document.getElementsByClassName("remove-product-button")
 console.log(removeproduto)
 for (let i = 0; i < removeproduto.length; i++) {
-    removeproduto[i].addEventListener("click", function(event) {
-        event.target.parentElement.parentElement.remove()
-    })
+    removeproduto[i].addEventListener("click", removeProduct)}
+
+    function removeProduct (event) {
+    event.target.parentElement.parentElement.remove()
+        updateTotal()
+}
+function updateTotal() {
+let totalAmount = 0
+const produtoCarrinho = document.getElementsByClassName("cart-product")
+for (let i = 0; i < produtoCarrinho.length; i++) {
+const precoProduto = produtoCarrinho[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
+const qtdProduto = produtoCarrinho[i].getElementsByClassName("product-qtd-input")[0].value
+
+totalAmount += precoProduto * qtdProduto
+}
+totalAmount = totalAmount.toFixed(2)
+totalAmount = totalAmount.replace(".", ",")
+document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
 }
